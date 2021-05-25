aa40ca7e33a6133029f0f7060b3b1a783c86ca38
RustiStefan <63097644+RustiStefan@users.noreply.github.com>
2021-05-25 10:55:49 +0300

    Create ConectareBD.java

---
+++ BD/ConectareBD.java
@@ -0,0 +1,27 @@
+package BD;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static Connection con = null;
+
+    static
+    {
+        String url = "jdbc:mysql://localhost:3306/BD";
+        String user = "Stefan";
+        String pass = "123";
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(url, user, pass);
+        }
+        catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static Connection getConnection()
+    {
+        return con;
+    }
+}
